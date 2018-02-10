8858f004d568e6d09757274276b2713b88bd3887
recrit <recrit@gmail.com>
2018-02-10 12:31:29 -0500

    modal dialog: fix js errors, set height statically on initialize since waiting for content load has cross browser issues

--- js/dialog.js
+++ js/dialog.js
@@ -3,23 +3,10 @@
  * Paragraphs Previewer handling.
  */
 
-(function ($, Drupal, drupalSettings) {
-
+(function ($, Drupal, window) {
   'use strict';
 
   var previewer = {};
-
-  /**
-   * Reset extended dialog properties.
-   *
-   * @param Drupal.dialog dialog
-   *   The dialog object
-   */
-  previewer.dialogReset = function(dialog) {
-    dialog.isLoading = false;
-    dialog.loadedCount = 0;
-    dialog.loadableCount = 0;
-  };
 
   /**
    * Set the initial dialog settings based on client side information.
@@ -32,21 +19,18 @@
    *   Optional The combined dialog settings.
    */
   previewer.dialogInitialize = function(dialog, $element, settings) {
-    dialog.isLoading = true;
-    dialog.loadedCount = 0;
-    dialog.loadableCount = 0;
-
     var windowHeight = $(window).height();
     if (windowHeight > 0) {
       // Set maxHeight based on calculated pixels.
       // Setting a relative value (100%) server side did not allow scrolling
       // within the modal.
       settings.maxHeight = windowHeight;
+      settings.height = 0.98 * windowHeight;
     }
   };
 
   /**
-   * Set the dialog settings based on the content.
+   * Dialog after create listener.
    *
    * @param Drupal.dialog dialog
    *   The dialog object
@@ -55,68 +39,36 @@
    * @param object $settings
    *   The combined dialog settings.
    */
-  previewer.dialogUpdateForContent = function(dialog, $element, settings) {
-    if (!dialog.isLoading && settings.maxHeight) {
-      var $content = $('.paragraphs-previewer-iframe', $element).contents().find('body');
+  previewer.onDialogAfterCreate = function(dialog, $element, settings) {
+    // Set body class to disable scrolling.
+    $('body').addClass('paragraphs-previewer-dialog-active');
 
-      if ($content.length) {
-        // Fit content.
-        var contentHeight = $content.outerHeight();
-        var modalContentContainerHeight = $element.height();
-
-        var fitHeight;
-        if (contentHeight < modalContentContainerHeight) {
-          var modalHeight = $element.parent().outerHeight();
-          var modalNonContentHeight = modalHeight - modalContentContainerHeight
-          fitHeight = contentHeight + modalNonContentHeight
-        }
-        else {
-          fitHeight = 0.98 * settings.maxHeight;
-        }
-
-        // Set to the new height bounded by min and max.
-        var newHeight = fitHeight;
-        if (fitHeight < settings.minHeight) {
-           newHeight = settings.minHeight;
-        }
-        else if (fitHeight > settings.maxHeight) {
-          newHeight = settings.maxHeight;
-        }
-        settings.height = newHeight;
-        $element.dialog('option', 'height', settings.height);
-      }
-    }
-  };
-  
-  /**
-   * Load listener to set content height.
-   *
-   * @param Drupal.dialog dialog
-   *   The dialog object
-   * @param jQuery $element
-   *   The element jQuery object.
-   * @param object $settings
-   *   The combined dialog settings.
-   */
-  previewer.dialogLoader = function(dialog, $element, settings) {
-    var $loadedElements = $('iframe, img, video', $element);
-    dialog.loadableCount = $loadedElements.length;
-    if (dialog.loadableCount) {
-      // Update settings after all content is loaded.
-      $loadedElements.load(function(loadEvent) {
-        dialog.loadedCount++;
-        if (dialog.loadedCount == dialog.loadableCount) {
-          dialog.isLoading = false;
-          $element.addClass('paragraphs-previewer-dialog-loaded')
-          previewer.dialogUpdateForContent(dialog, $element, settings);
+    var $loadables = $('iframe', $element);
+    if ($loadables.length > 0) {
+      // Add class after loaded.
+      $loadables.on('load', function () {
+        if (!$element.hasClass('paragraphs-previewer-dialog-loaded')) {
+          $element.addClass('paragraphs-previewer-dialog-loaded');
         }
       });
     }
     else {
-      // No update needed since height is 'auto'.
-      dialog.isLoading = false;
-      $element.addClass('paragraphs-previewer-dialog-loaded')
+      // Nothing to load.
+      $element.addClass('paragraphs-previewer-dialog-loaded');
     }
+  };
+
+  /**
+   * Dialog after close listener.
+   *
+   * @param Drupal.dialog dialog
+   *   The dialog object
+   * @param jQuery $element
+   *   The element jQuery object.
+   */
+  previewer.onDialogAfterClose = function(dialog, $element) {
+    // Remove body class to enable scrolling in the parent window.
+    $('body').removeClass('paragraphs-previewer-dialog-active');
   };
 
   /**
@@ -141,35 +93,31 @@
       dialogClass = $element.dialog('option', 'dialogClass');
     }
 
-    return dialogClass && dialogClass.indexOf('paragraphs-previewer-ui-dialog') > -1;
+    return typeof dialogClass !== 'undefined' && dialogClass.indexOf('paragraphs-previewer-ui-dialog') > -1;
   };
 
-  // Dialog listeners.
-  $(window).on({
-    'dialog:beforecreate': function (event, dialog, $element, settings) {
-      if (previewer.dialogIsPreviewer(dialog, $element, settings)) {
-        // Initialize the dialog.
-        previewer.dialogInitialize(dialog, $element, settings);
-      }
-    },
-    'dialog:aftercreate': function (event, dialog, $element, settings) {
-      if (previewer.dialogIsPreviewer(dialog, $element, settings)) {
-        // Set body class to disable scrolling.
-        $('body').addClass('paragraphs-previewer-dialog-active');
-
-        // Adjust dialog after all content is loaded.
-        previewer.dialogLoader(dialog, $element, settings);
-      }
-    },
-    'dialog:afterclose': function (event, dialog, $element) {
-      if (previewer.dialogIsPreviewer(dialog, $element)) {
-        // Reset extended properties.
-        previewer.dialogReset(dialog);
-
-        // Remove body class to enable scrolling in the parent window.
-        $('body').removeClass('paragraphs-previewer-dialog-active');
-      }
+  // Drupal behaviors.
+  Drupal.behaviors.paragraphsPreviewer = {
+    attach: function (context) {
+      // Dialog listeners.
+      $(window).on({
+        'dialog:beforecreate': function (event, dialog, $element, settings) {
+          if (previewer.dialogIsPreviewer(dialog, $element, settings)) {
+            previewer.dialogInitialize(dialog, $element, settings);
+          }
+        },
+        'dialog:aftercreate': function (event, dialog, $element, settings) {
+          if (previewer.dialogIsPreviewer(dialog, $element, settings)) {
+            previewer.onDialogAfterCreate(dialog, $element, settings);
+          }
+        },
+        'dialog:afterclose': function (event, dialog, $element) {
+          if (previewer.dialogIsPreviewer(dialog, $element)) {
+            previewer.onDialogAfterClose(dialog, $element);
+          }
+        }
+      });
     }
-  });
+  };
 
-})(jQuery, Drupal, drupalSettings);
+})(jQuery, Drupal, window);
